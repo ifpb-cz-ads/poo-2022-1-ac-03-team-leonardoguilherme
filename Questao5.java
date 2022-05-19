1f9a9c4f4629f5d57a71c48456d60e9644f2cbd3
Guilherme Costa <gui-costaquino@hotmail.com>
2022-05-18 21:18:47 -0300

    quinta questão

---
+++ Questao5.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Questao5{
+    public static void main(String[] args){
+        Scanner ler = new Scanner(System.in);
+        int num;
+        String dias[] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+        System.out.println("Informe um número de 1 a 7: ");
+        num = ler.nextInt();
+        switch(num){
+            case 1: System.out.println(dias[0]);
+            break;
+            case 2: System.out.println(dias[1]);
+            break;
+            case 3: System.out.println(dias[2]);
+            break;
+            case 4: System.out.println(dias[3]);
+            break;
+            case 5: System.out.println(dias[4]);
+            break;
+            case 6: System.out.println(dias[5]);
+            break;
+            case 7: System.out.println(dias[6]);
+            break;
+            default: System.out.println("Número inválido.");
+        }
+    }
+}
